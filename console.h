9f4efba510bbe0202aae3ce25091961574f8aaf4
Michel Kakulphimp <michel@kakulphimp.net>
2023-06-10 23:11:37 -0700

    Update console.h

--- console.h
+++ console.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 Michel Kakulphimp
+ * Copyright (c) 2023 Michel Kakulphimp
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,6 +29,9 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+#include <stdbool.h>
+
 // Console ANSI colors
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -42,7 +45,7 @@ extern "C" {
 typedef enum
 {
     ERROR = -1,
-    SUCCESS =   0,
+    SUCCESS = 0,
 } FunctionResult_e;
 
 typedef enum
@@ -53,8 +56,8 @@ typedef enum
 
 typedef enum
 {
-    ORIENTATION_V = 1 << 0, // Default is options horizontal
-    NO_DIVIDERS = 1 << 1, // Default is dividers drawn
+    ORIENTATION_V   = 1 << 0, // Default is options horizontal
+    NO_DIVIDERS     = 1 << 1, // Default is dividers drawn
 } ConsoleOptionFlags_e;
 
 typedef enum
@@ -66,18 +69,24 @@ typedef enum
     LOGGING_LEVEL_3 = 3, 
 } LoggingLevel_e;
 
-#define DBL_LINE_CHAR               "═"
-#define SGL_LINE_CHAR               "─"
+// These are unicode, so they're stored as strings since they don't fit in char
+// #define DBL_LINE_CHAR               "═"
+// #define SGL_LINE_CHAR               "─"\
+// MMK 230226 - switching to plain old ASCII as the logs aren't loving these unicode characters
+#define DBL_LINE_CHAR               "="
+#define SGL_LINE_CHAR               "-"
 
 #define NO_TOP_MENU                 (0) // NULL
 #define NO_SUB_MENU                 (0) // NULL
 #define NO_FUNCTION_POINTER         (0) // NULL
 #define NO_ARGS                     (0) // NULL
 #define NO_SIZE                     (0)
-#define MAX_MENU_NAME_LENGTH        (40)
-#define MAX_MENU_DESCRIPTION_LENGTH (40)
+#define MAX_MENU_NAME_LENGTH        (CONSOLE_WIDTH - 40)
+#define MAX_MENU_DESCRIPTION_LENGTH (CONSOLE_WIDTH - 40)
 #define CONSOLE_WIDTH               (120)
-#define STRING_BUFFER_SIZE          (CONSOLE_WIDTH + 20)
+#define TEXT_BLOCK_SIZE             (CONSOLE_WIDTH - 40)
+#define STRING_BUFFER_SIZE          (CONSOLE_WIDTH + 100)
+#define NUM_STRING_BUFFERS          (5)
 #define HEADER_TITLE_EXTRAS_WIDTH   (6) // "=[  ]=" = 6 characters
 #define MAX_HEADER_TITLE_WIDTH      (CONSOLE_WIDTH - HEADER_TITLE_EXTRAS_WIDTH) 
 #define PAGE_LENGTH                 (10) // Maximum length of a page (0-9)
@@ -94,6 +103,9 @@ typedef enum
 // User should define a splash screen as an as array of const pointer to const char.
 typedef const char *const Splash_t[];
 
+// Typedef the console.h function pointer
+typedef FunctionResult_e (*ConsoleFunctionPointer_t)(int argc, char *argv[]);
+
 typedef struct ConsoleMenuId
 {
     char                name[MAX_MENU_NAME_LENGTH];
@@ -104,7 +116,7 @@ typedef struct ConsoleMenuItem
 {
     ConsoleMenuId_t     id;
     struct ConsoleMenu  *sub_menu;
-    FunctionResult_e    (*function_pointer)(int argc, char *argv[]);
+    ConsoleFunctionPointer_t function_pointer;
 } ConsoleMenuItem_t;
 
 typedef struct ConsoleMenu
@@ -124,6 +136,8 @@ typedef struct ConsoleSelection
     const char          *description;
 } ConsoleSelection_t;
 
+typedef ConsoleSelection_t ConsoleSelections[];
+
 typedef struct ConsoleSettings
 {
     // Splash screen settings
@@ -131,36 +145,53 @@ typedef struct ConsoleSettings
     unsigned int        num_splash_lines;
     // Pointer to the main menu
     ConsoleMenu_t       *main_menu_pointer;
+    // Small Headers
+    bool                small_headers;
     // Logging level
     LoggingLevel_e      logging_level;
 } ConsoleSettings_t;
 
-void Console_Init(ConsoleSettings_t *settings);
-void Console_Main(void);
+void ConsoleInit(ConsoleSettings_t *settings);
+void ConsoleMain(void);
 
-void Console_PromptForAnyKeyBlocking(void);
-char Console_CheckForKeyBlocking(void);
-char Console_CheckForKey(void);
-unsigned int Console_PromptForInt(const char *prompt);
+void ConsoleSmallHeaders(bool enable);
 
-void Console_TraverseMenus(ConsoleMenu_t *menu);
-char Console_PrintOptionsAndGetResponse(const ConsoleSelection_t selections[], unsigned int num_selections, unsigned int num_menu_selections, unsigned int option_flags);
-void Console_Print(LoggingLevel_e logging_level, const char *format, ...);
-void Console_PrintNoEol(LoggingLevel_e logging_level, const char *format, ...);
-void Console_PrintNewLine(LoggingLevel_e logging_level);
-void Console_PrintHeader(LoggingLevel_e logging_level, const char *header_string);
-void Console_PrintDivider(LoggingLevel_e logging_level);
-void Console_PrintMenu(ConsoleMenu_t *menu);
+void ConsolePromptForAnyKeyBlocking(void);
+char ConsoleCheckForKeyBlocking(void);
+char ConsoleCheckForKey(void);
+
+uint32_t ConsolePromptForHexUInt32(const char *prompt, uint32_t default_val);
+uint64_t ConsolePromptForHexUInt64(const char *prompt, uint64_t default_val);
+unsigned int ConsolePromptForInt(const char *prompt, unsigned int default_val);
+char *ConsolePromptForString(const char *prompt, const char *default_val);
+
+void ConsoleTraverseMenus(ConsoleMenu_t *menu);
+char ConsolePrintOptionsAndGetResponse(const ConsoleSelection_t selections[], unsigned int num_selections, unsigned int num_menu_selections, unsigned int option_flags);
+void ConsolePrint(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintInPlace(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintBlock(LoggingLevel_e logging_level, const char *block_string);
+void ConsolePrintColor(LoggingLevel_e logging_level, const char *color_string, const char *inner_string);
+void ConsolePrintError(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintWarn(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintSuccess(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintNoEol(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintNewLine(LoggingLevel_e logging_level);
+void ConsolePrintHeader(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintSubHeader(LoggingLevel_e logging_level, const char *format, ...);
+void ConsolePrintHeaderInternal(LoggingLevel_e logging_level, const char *ruler_string, const char *header_string);
+void ConsolePrintDivider(LoggingLevel_e logging_level);
+void ConsolePrintMenu(ConsoleMenu_t *menu);
+unsigned int ConsoleGetStringBufferIndex(void);
 
 // Fundamental functions wrapped around the logging level
-char Console_GetCharInternal(LoggingLevel_e logging_level);
-void Console_PutCharInternal(LoggingLevel_e logging_level, char c);
-void Console_PutStringInternal(LoggingLevel_e logging_level, const char * string);
+char ConsoleGetCharInternal(LoggingLevel_e logging_level);
+void ConsolePutCharInternal(LoggingLevel_e logging_level, char c);
+void ConsolePutStringInternal(LoggingLevel_e logging_level, const char * string);
 
 // Platform-specific functions that must be implemented
-extern char Console_GetChar(void);
-extern void Console_PutChar(char c);
-extern void Console_PutString(const char * string);
+extern char ConsoleGetChar(void);
+extern void ConsolePutChar(char c);
+extern void ConsolePutString(const char * string);
 
 #ifdef __cplusplus
 }
